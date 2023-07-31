5cefb1c9110a522f6eb7f022e0fccc02853b0926
bob <robert.matthew.brooks@gmail.com>
2023-08-01 00:33:28 +0100

    messing with maps

---
+++ spiking/flutter_maps/lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_animations/flutter_map_animations.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: MapWidget(),
+      ),
+    );
+  }
+}
+
+class MapWidget extends StatefulWidget {
+  const MapWidget({super.key});
+
+  @override
+  State<MapWidget> createState() => _MapWidgetState();
+}
+
+class _MapWidgetState extends State<MapWidget> with TickerProviderStateMixin {
+  late final mapController = AnimatedMapController(
+    vsync: this,
+    duration: const Duration(milliseconds: 1000),
+    curve: Curves.easeInOut,
+  );
+
+  MaterialColor lovelyColour = Colors.purple;
+  String markerText = 'clickable marker';
+
+  final points = [
+    const LatLng(51.6, -0.13),
+    const LatLng(51.4, -0.12),
+    const LatLng(51.5, -0.3),
+    const LatLng(51.3, 0.11),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      mapController: mapController.mapController,
+      options: MapOptions(
+        minZoom: 2.0,
+        maxZoom: 18.0,
+        center: const LatLng(53.2043, -1.1549),
+        interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate,
+        zoom: 9.2,
+        onMapReady: () {
+          mapController.mapController.mapEventStream.listen((evt) {});
+        },
+      ),
+      nonRotatedChildren: [
+        Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                mapController.animateTo(
+                    dest: const LatLng(51.509364, -0.128928), zoom: 9.2);
+              },
+              child: const Text('Go to London'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                mapController.animateTo(
+                    dest: const LatLng(53.2043, -1.1549), zoom: 9.2);
+              },
+              child: const Text('Go to Warsop'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                mapController.animateTo(
+                    dest: const LatLng(53.3811, -1.4701), zoom: 9.2);
+              },
+              child: const Text('Go to Sheffield'),
+            ),
+          ],
+        ),
+        RichAttributionWidget(
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () =>
+                  launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+            ),
+          ],
+        ),
+      ],
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+        MarkerLayer(
+          markers: [
+            Marker(
+              point: const LatLng(51.509364, -0.128928),
+              width: 80,
+              height: 80,
+              builder: (context) => GestureDetector(
+                onTap: () {
+                  debugPrint('you clicked a thing');
+                  setState(() {
+                    lovelyColour = (lovelyColour == Colors.purple
+                        ? Colors.orange
+                        : Colors.purple);
+                    markerText = 'redirect?';
+                  });
+                },
+                child: Column(
+                  children: [
+                    const Icon(Icons.location_on, color: Colors.red, size: 30),
+                    Text(
+                      markerText,
+                      style: TextStyle(
+                        color: Colors.white,
+                        backgroundColor: lovelyColour,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ...points.map(
+              (point) => Marker(
+                point: point,
+                width: 80,
+                height: 80,
+                builder: (context) => const Icon(Icons.location_on, size: 30),
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
